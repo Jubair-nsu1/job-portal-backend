3371a8b4c110cc2bc963c5fe1783989fd2b37b27
Jubair Ahmed <62503637+Jubair-nsu1@users.noreply.github.com>
2024-11-10 13:44:08 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,7 +4,8 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
       //'mongodb://127.0.0.1:27017/job-portal',
-       'mongodb://mongodb:27017/job-portal',
+      //'mongodb://mongodb:27017/job-portal',
+      'mongodb+srv://jubair421:mango123@cluster0.ap8mvgi.mongodb.net/job-portal',
       // process.env.MONGO_URI_LOCAL,
       // 'mongodb+srv://jubair421:mango123@cluster0.ap8mvgi.mongodb.net/',
       // process.env.MONGO_URI_DOCKER,
